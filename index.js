6aeedcc419e7dc77ef9b35f18f87804b145001fe
titophil <kipronotitus254@gmail.com>
2025-03-04 10:17:44 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    "HELLO!".toUpperCase();
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    "HELLO".toLowerCase();
+    return string.toLowerCase();
+}
+function logShout(string) {
+      console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+if (string===string.toUpperCase()){
+    return "YES INDEED!";
+}
+else if (string===string.toLowerCase()){
+    return "I can't hear you!"
+}
+
+else (string==="let's have dinner!")
+return ("I would love to!");
+}
